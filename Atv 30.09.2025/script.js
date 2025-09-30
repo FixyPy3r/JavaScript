4536888d5b3864c4d21577f8f5b0590221d896b4
Vitor Emanuel <Vitor00180066100@gmail.com>
2025-09-30 11:50:19 -0300

    Add files via upload

---
+++ Atv 30.09.2025/script.js
@@ -0,0 +1,21 @@
+// array -> vetores, matrizes
+// estrutura de dados para armazenar um conjunto de elementos
+
+let frutas = ["Banana", "Maça", "Melancia"]; // array de tamanho 3
+console.log(frutas);
+console.log(frutas[1])
+
+let numeros = new Array(2,8,9,10);
+console.log(numeros);
+
+let arrayVazio = [];
+console.log(arrayVazio);
+arrayVazio[0] = "Gaby";
+console.log(arrayVazio);
+
+// Tamanho do array -> length
+let dados = [10,50,30]; // tamanho 5 -> lenght 3
+console.log(dados.lenght);
+console.log(dados[1]);
+let ultimoElemento = dados.length - 1;
+console.log(ultimoElemento);
